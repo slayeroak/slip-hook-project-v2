0b9e398dfb45a54370b90e481a4b784c51318ebb
slayeroak <roakley1@villanova.edu>
2020-05-14 14:07:24 -0400

    virtual fields

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema(
     {
@@ -41,9 +42,43 @@ const userSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-// virtual fiels
+// virtual fields
+userSchema
+    .virtual('password')
+    .set(function(password) {
+        // create temp variable called _password
+        this._password = password;
+        // generate salt
+        this.salt = this.makeSalt();
+        // encrypt password
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
 // methods > authenticate, encryptPassword, makeSalt
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+    encryptPassword: function(password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    },
+
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    }
+};
 // export user model
 
-
-
+module.exports = mongoose.model('User', userSchema);
