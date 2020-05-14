531aade1dd4161c764947fdf911459174cb6b75a
slayeroak <roakley1@villanova.edu>
2020-05-14 14:22:36 -0400

    npm packages, added to server, env

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 // import routes
 const authRoutes = require('./routes/auth');
+
+// app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
 
 // middlewares
 app.use('/api', authRoutes);
