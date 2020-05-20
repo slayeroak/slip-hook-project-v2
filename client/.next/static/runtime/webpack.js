9e76c25ecf59caff723ee33d80388bb4375512a2
slayeroak <roakley1@villanova.edu>
2020-05-20 15:06:43 -0400

    added update to route to fix duplicating cat on a cat  update bug

--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ffab3d55dfab0189a256";
+/******/ 	var hotCurrentHash = "722249931d1a8f6243c2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- server/routes/category.js
+++ server/routes/category.js
@@ -13,7 +13,7 @@ const { create, list, read, update, remove } = require('../controllers/category'
 router.post('/category', categoryCreateValidator, runValidation, requireSignin, adminMiddleware, create);
 router.get('/categories', list);
 router.post('/category/:slug', read);
-router.put('/category/:slug', categoryUpdateValidator, runValidation, requireSignin, adminMiddleware, create);
+router.put('/category/:slug', categoryUpdateValidator, runValidation, requireSignin, adminMiddleware, update);
 router.delete('/category/:slug', requireSignin, adminMiddleware, remove);
 
 module.exports = router;
